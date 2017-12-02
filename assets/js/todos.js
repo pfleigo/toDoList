8626f97d2a7e0824589b55f1ce0f5eebb1f62e90
pfleigi <pfleigi@gmail.com>
2017-12-02 20:25:45 +0100

    file upload

---
+++ assets/js/todos.js
@@ -0,0 +1,24 @@
+//Check off specific todos by clicking
+$("ul").on("click", "li", function(){
+	$(this).toggleClass("completed");
+});
+
+//click on X to delete
+$("ul").on("click", "span", function(event){
+	$(this).parent().fadeOut(500, function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
+});
+
+//add event listener for input of new to dos
+$("input[type='text']").keypress(function(event){
+	if(event.which === 13){
+		//grabbing text from input
+		let todoText = $(this).val()
+		//delete input field
+		$(this).val("");
+		//create a new li and add to ul
+		$("ul").append("<li><span><i class='fa fa-trash' aria-hidden='true'></i></span> " + todoText + "</li>");
+	}
+});
